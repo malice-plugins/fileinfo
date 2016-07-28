75128ca6462afff7a41554be0dc1fdd6535112aa
blacktop <blacktop@users.noreply.github.com>
2016-07-27 21:48:37 -0600

    update dockerignore

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,9 +6,10 @@ if [[ "$1" == -* ]]; then
 	set -- info "$@"
 fi
 
-# Run as user "info" if the command is "info"
+# Run as user "malice" if the command is "info"
 if [ "$1" = 'info' ]; then
 	set -- gosu malice tini -- "$@"
 fi
 
+# else default to run whatever the user wanted like "bash"
 exec "$@"
